76a081ea6df3b70f5df8156e8c50ea4cbf4342c1
shreyan-collab <shdh7090@colorado.edu>
2022-01-16 23:20:37 -0700

    Commit3-Added Comments and error check in writer.sh file

---
+++ finder-app/finder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# @File name :finder.sh
+# @brief     :Finder script to find the occurence of a string within
+#	       a directory
+# @author    :Shreyan Prabhu 01/14/2022
+# @References: 1) https://ryanstutorials.net/bash-scripting-tutorial/bash-variables.php 
+#	       2) https://ryanstutorials.net/bash-scripting-tutorial/bash-if-statements.php
+
+errormessage="ERROR: Invalid Number of arguments\nTotal number of arguments must be 2.\nThe order of arguments should be\n1.Path of the directory\n2.String to be searched within the directory"
+
+if [ ! $# -eq 2 ]		#Checking if number of arguments is 2
+then
+	echo -e $errormessage
+	exit 1
+fi
+
+directory="$1"			#Checking if the directory is valid
+if [ ! -d "$directory" ]
+then 
+	echo It is not a directory
+	exit 1
+fi
+
+word=$2
+cd $directory
+totallines=$(grep -r $word . | wc -l)	#Checking the entire directory
+totalfiles=$(find -type f | wc -l)    #Using pipe to count files
+echo The number of files are $totalfiles and the number of matching lines are $totallines
---
+++ finder-app/writer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# @File name :finder.sh
+# @brief     :Writer script to create a file in specific directory and the 
+# @author    :Shreyan Prabhu 01/14/2022
+# @References: 1) https://ryanstutorials.net/bash-scripting-tutorial/bash-variables.php 
+#	       2) https://ryanstutorials.net/bash-scripting-tutorial/bash-if-statements.php
+
+
+errormessage="ERROR: Invalid Number of arguments\nTotal number of arguments must be 2.\nThe order of arguments should be\n1.File path \n2.String to be written to the path"
+
+if [ ! $# -eq 2 ]		 #Checking if number of arguments is 2
+then
+	echo -e $errormessage
+	exit 1
+fi
+
+fullpath="$1"
+directory="$(dirname "${fullpath}")"	#Extracting directory from path
+filename="$(basename "${fullpath}")"	#Extracting filename from path
+mkdir -p $directory && touch $fullpath
+cd $directory
+if [ ! -d "$directory" ]
+then 
+	echo It is not a directory	#Checking if the directory is valid
+	exit 1
+fi
+if [ ! -f "$fullpath" ]
+then
+	echo "File did not exist"	#Checking if the file is created
+	exit 1
+fi
+
+word=$2
+echo $word > $filename
+if [ ! $? -eq 0 ]			#Check if last command is executed
+then 
+	echo "The input string is not written to the file"
+	exit 1
+fi
